26478f94ea2a4a33193360b71f011ffc74a71849
Matt Lewis <mlewis@DEBIANDEVLEWIS>
2015-07-25 23:20:59 -0400

    Added Run.sh script file

---
+++ Run.sh
@@ -0,0 +1,5 @@
+echo "Running Json Test Program"
+perl json.pl > output.txt
+echo "Program Complete!"
+echo "Please Check output.txt For Stdout Messages"
+echo "Please Check students_and_teachers.csv For Required CSV File"
